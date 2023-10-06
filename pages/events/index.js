66b37ceac41c3e483ad04b348a9594160deda125
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2023-10-06 18:54:40 +0300

    Filter-logic

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
 import { Fragment } from "react";
 import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/EvenetList";
 import EventSearch from "../../components/events/EventSearch";
 
 const EventsPage = () => {
+  const router = useRouter();
   const evnets = getAllEvents();
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath);
+  }
+  
   return (
     <Fragment>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler}/>
       <EventList items={evnets}/>
     </Fragment>
   );
